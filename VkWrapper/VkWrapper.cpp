5ab5865564868e2e029af136a94d02fc0bfc0a25
Clematrics <clematrics.studio@gmail.com>
2016-03-31 19:00:26 +0200

    Correction d'erreur de synchronisation de projet

---
+++ VkWrapper/VkWrapper.cpp
@@ -0,0 +1,19 @@
+// Il s'agit du fichier DLL principal.
+
+//#include "stdafx.h"
+//
+//#include "VkWrapper.h"
+//#include "Lib/vulkan.h"
+//
+//
+//namespace VkWrapper {
+//
+//	VkResult Vk::CreateDevice(	VkPhysicalDevice                            physicalDevice,
+//								const VkDeviceCreateInfo*                   pCreateInfo,
+//								const VkAllocationCallbacks*                pAllocator,
+//								VkDevice*                                   pDevice) {
+//		return vkCreateDevice(physicalDevice, pCreateInfo, pAllocator, pDevice);
+//	};
+//
+//}
+
---
+++ VkWrapper/VkWrapper.h
@@ -0,0 +1,17 @@
+// VkWrapper.h
+
+#pragma once
+
+//using namespace System;
+//
+//namespace VkWrapper {
+//
+//	public ref class Vk
+//	{
+//	public:
+//		virtual VkResult Vk::CreateDevice(	VkPhysicalDevice                            physicalDevice,
+//								const VkDeviceCreateInfo*                   pCreateInfo,
+//								const VkAllocationCallbacks*                pAllocator,
+//								VkDevice*                                   pDevice) abstract;
+//	};
+//}
