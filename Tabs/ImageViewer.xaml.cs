17fcc06bed2cd446b18a2fd3fec8daf57ed93dd3
flobasse <misterf.rio68@gmail.com>
2016-04-08 10:22:47 +0200

    Implémentation du déplacement de l'image

--- Tabs/ImageViewer.xaml.cs
+++ Tabs/ImageViewer.xaml.cs
@@ -22,7 +22,58 @@ namespace SkyView.Tabs {
             InitializeComponent();
         }
 
+        #region Déplacement de l'image dans l'éditeur
 
+        private Point currentGraphPoint = new Point(0, 0);
+        private Point startDragPoint;
+        private Point vectorDragPoint;
+        private bool IsGraphMoving = false;
+
+        private void image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed) return;
+
+            image.CaptureMouse();
+            startDragPoint = Mouse.GetPosition(this);
+            IsGraphMoving = true;
+        }
+        private void image_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsGraphMoving) return;
+
+            vectorDragPoint = (Point)(Mouse.GetPosition(this) - startDragPoint);
+            currentGraphPoint = currentGraphPoint + (Vector)vectorDragPoint;
+            CheckAndUpdateImage();
+
+            startDragPoint = Mouse.GetPosition(this);
+        }
+        private void image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Released) return;
+
+            image.ReleaseMouseCapture();
+            IsGraphMoving = false;
+        }
+
+        private void NodeEditor_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            CheckAndUpdateImage();
+        }
+
+        private void CheckAndUpdateImage()
+        {
+            if (currentGraphPoint.X > 0)
+                currentGraphPoint.X = 0;
+            if (currentGraphPoint.X < imageCanvas.ActualWidth - 4096)
+                currentGraphPoint.X = imageCanvas.ActualWidth - 4096;
+            if (currentGraphPoint.Y > 0)
+                currentGraphPoint.Y = 0;
+            if (currentGraphPoint.Y < imageCanvas.ActualHeight - 4096)
+                currentGraphPoint.Y = imageCanvas.ActualHeight - 4096;
+            Canvas.SetTop(image, currentGraphPoint.Y);
+            Canvas.SetLeft(image, currentGraphPoint.X);
+        }
+        #endregion
 
     }
 }
