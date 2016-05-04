fddf6189ec6f352105a702efa96de3d30e4400ff
Clematrics <clematrics.studio@gmail.com>
2016-05-04 16:09:25 +0200

    Ajout de la classe image

---
+++ Image/Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyView.Image {
+    public class Image {
+
+
+    }
+}
