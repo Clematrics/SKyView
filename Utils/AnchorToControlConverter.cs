76113443e6d296b5cbb4abd42ce48d795cd39da9
Clematrics <clematrics.studio@gmail.com>
2016-05-25 17:01:12 +0200

    Correction de l'affichage des liens aux formes Bézier

--- Utils/AnchorToControlConverter.cs
+++ Utils/AnchorToControlConverter.cs
@@ -10,30 +10,20 @@ using System.Windows.Data;
 
 namespace SkyView.Utils
 {
-    public class AnchorToControlConverter : IValueConverter
-    {
-        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if(value is System.Windows.Point)
-
-            {
-                if (parameter is int && (int)parameter == 100)
-                {
+    public class AnchorToControlConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value is System.Windows.Point) {
+                if (parameter is int && (int)parameter == 100) {
                     return (System.Windows.Point)value + new Vector(100, 0);
                 }
-           
-               else if (parameter is int && (int)parameter == -100)
-                {
+                else if (parameter is int && (int)parameter == -100) {
                     return (System.Windows.Point)value + new Vector(-100, 0);
                 }
             }
-
             return null;
         }
 
-
-        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
             throw new NotImplementedException();
         }
     }
